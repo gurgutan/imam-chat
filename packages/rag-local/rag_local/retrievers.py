83d7d87471776f082f8ad36fb6e6105c6f20c6ce
Ivan Slepovichev <gurgutan@yandex.ru>
2024-04-05 23:11:34 +0400

    chroma persistance bugfix

--- packages/rag-local/rag_local/retrievers.py
+++ packages/rag-local/rag_local/retrievers.py
@@ -25,22 +25,33 @@ class ChromaRetreiverComponent:
         rebuild: bool = False,
         **kwargs,
     ) -> VectorStore:
+        client_settings = Settings(
+            is_persistent=True, anonymized_telemetry=False, persist_directory=path
+        )
         # TODO: Create Document check befor adding db and remove next lines (del of db)
+        collection_name = "rag-local"
         if os.path.isdir(path) and rebuild:
             try:
                 shutil.rmtree(path)
             except OSError as e:
                 logger.error("Error: %s - %s", e.filename, e.strerror)
-        client_settings = Settings(
-            is_persistent=True, anonymized_telemetry=False, persist_directory=path
-        )
-        if documents:
-            return Chroma.from_documents(
+
+        if not os.path.isdir(path):
+            assert documents, "Error: no documents"
+            logger.info("Vector db not found. Building indexes...")
+            store = Chroma.from_documents(
+                persist_directory=path,
                 documents=documents,
-                collection_name="rag-local",
+                collection_name=collection_name,
                 embedding=embedder,
                 client_settings=client_settings,
-            ).as_retriever(search_kwargs=search_kwargs)
-        # TODO: from_db
+            )
         else:
-            raise NotImplementedError("Not implemented persistent vectorstore")
+            store = Chroma(
+                persist_directory=path,
+                collection_name=collection_name,
+                embedding_function=embedder,
+                client_settings=client_settings,
+            )
+
+        return store.as_retriever(search_kwargs=search_kwargs)
